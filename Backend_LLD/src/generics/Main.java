dc3a4eb36a72098860bd83c06070c8f5565130fe
nagasaijairam <your_personal_email@example.com>
2025-12-09 19:03:14 +0530

    Generics 2

--- Backend_LLD/src/generics/Main.java
+++ Backend_LLD/src/generics/Main.java
@@ -1,5 +1,9 @@
 package generics;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 //        Pair pair = new Pair();
@@ -15,5 +19,34 @@ public class Main {
         EvenPair.first =2;
         EvenPair.second =true;
         System.out.println(EvenPair.first + " is an Even number --> " + EvenPair.second);
+
+        //RAW TYPE
+        HashMap hashMap = new HashMap();
+        hashMap.put("Deepak", 80.0);
+        hashMap.put(80.0, "Deepak");
+
+        //Java 5 -> Generics were introduced.
+        // Java is a Backward Compatible language.
+
+        HashMap<String, Integer> hashMap1 = new HashMap<>();
+        //hashMap1.p
+
+        List<Animal> animals = new ArrayList<>();
+        List<Dog> dogs = new ArrayList<>();
+        List<Cat> cats = new ArrayList<>();
+
+        doSomething(animals);
+        doSomething(dogs);
+
+        //List<Animal> animals1 = new ArrayList<Dog>(); -> Not possible
+//        List<? extends Animal> animals1 = new ArrayList<Dog>(); --> possibele
+
+//        List<? extends Animal> list = new ArrayList<>();
+//        Animal an = new Dog();
+//        list.add(an);
+    }
+
+    private static void doSomething(List<? extends Animal> animals) {
+        System.out.println("Inside dosomething function");
     }
 }
