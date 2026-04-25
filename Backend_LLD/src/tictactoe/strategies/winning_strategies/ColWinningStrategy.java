2b933a17fef850a8eca23a811846cfc5d47cacef
nagasaijairam <your_personal_email@example.com>
2026-04-25 17:27:19 +0530

    Added col and digonal winning stratigies

--- Backend_LLD/src/tictactoe/strategies/winning_strategies/ColWinningStrategy.java
+++ Backend_LLD/src/tictactoe/strategies/winning_strategies/ColWinningStrategy.java
@@ -2,10 +2,27 @@ package tictactoe.strategies.winning_strategies;
 
 import tictactoe.models.Board;
 import tictactoe.models.Move;
+import tictactoe.models.Symbol;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ColWinningStrategy implements WinningStrategy {
+    private final Map<Integer,Map<Character,Integer>> colMap = new HashMap<>();
+
     public boolean checkWinner(Board board, Move move) {
         System.out.println("In ColWinningStrategy");
-        return true;
+        int col = move.getCell().getCol();
+        Character aChar = move.getPlayer().getSymbol().getaChar();
+
+        //if it is the first move of the game, we have to create the new hash map for the col first.
+        if(!colMap.containsKey(col)){
+            colMap.put(col, new HashMap<>());
+        }
+
+
+        Map<Character,Integer> currentColMap = colMap.get(col);
+        currentColMap.put(aChar,currentColMap.getOrDefault(aChar,0)+1);
+        return currentColMap.get(aChar)==board.getDimension();
     }
 }
--- Backend_LLD/src/tictactoe/strategies/winning_strategies/DiagonalWinningStrategy.java
+++ Backend_LLD/src/tictactoe/strategies/winning_strategies/DiagonalWinningStrategy.java
@@ -2,9 +2,40 @@ package tictactoe.strategies.winning_strategies;
 import tictactoe.models.Move;
 import tictactoe.models.Board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DiagonalWinningStrategy implements WinningStrategy{
+    private final Map<Character,Integer> leftDiagonalMap = new HashMap<Character,Integer>();
+    private final Map<Character,Integer> rightDiagonalMap = new HashMap<Character,Integer>();
+
     public boolean checkWinner(Board board,Move move){
         System.out.println("In DiagonalWinningStrategy");
+        int row = move.getCell().getRow();
+        int col = move.getCell().getCol();
+        Character aChar = move.getPlayer().getSymbol().getaChar();
+
+        // left diagnols row == col
+        if (row == col){
+            leftDiagonalMap.put(aChar, leftDiagonalMap.getOrDefault(aChar, 0) + 1);
+        }
+
+        //right diagnols row+col = N-1;
+        if ((row+col) == board.getDimension()-1){
+            if(!rightDiagonalMap.containsKey(aChar)){
+                rightDiagonalMap.put(aChar,0);
+            }
+            rightDiagonalMap.put(aChar, rightDiagonalMap.get(aChar) + 1);
+        }
+
+        if((row == col)&& leftDiagonalMap.get(aChar)==(board.getDimension()) ){
+            return true;
+        }
+        if((row+col) == board.getDimension()-1 &&  rightDiagonalMap.get(aChar)==(board.getDimension()) ){
+            return true;
+        }
+
         return false;
+
     }
 }
